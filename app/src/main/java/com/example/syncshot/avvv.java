582b6e25e56e542404d9d61751300575e49ca086
udbhav17319 <33763903+udbhav17319@users.noreply.github.com>
2020-11-07 21:33:40 +0530

    clean

--- app/src/main/java/com/example/syncshot/avvv.java
+++ app/src/main/java/com/example/syncshot/avvv.java
@@ -1,9 +1,4 @@
 package com.example.syncshot;
 
 public class avvv {
-    System.out.println("avvv");
-    System.out.println("GfG1");
-    System.out.println("avvv");
-    System.out.println("GfG1");
-    System.out.println("raju");
 }
