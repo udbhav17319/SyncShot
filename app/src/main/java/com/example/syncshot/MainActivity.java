2a183d21b3d8a0b34e5d63456acb7dcbea1f79a6
udbhav17319 <33763903+udbhav17319@users.noreply.github.com>
2020-11-07 23:24:32 +0530

    add button

--- app/src/main/java/com/example/syncshot/MainActivity.java
+++ app/src/main/java/com/example/syncshot/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.syncshot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    Button add_people_button =findViewById(R.id.add_people_button);
+    //add_people_button.setOnClickListener(new View.OnClickListener()
+
 }
