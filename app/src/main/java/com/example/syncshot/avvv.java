857286f02b878d1805d400b35aa9a81fc75a9722
om-khandade <khandade18289@iiitd.ac.in>
2020-11-07 19:29:40 +0530

    copy print

--- app/src/main/java/com/example/syncshot/avvv.java
+++ app/src/main/java/com/example/syncshot/avvv.java
@@ -3,4 +3,6 @@ package com.example.syncshot;
 public class avvv {
     System.out.println("avvv");
     System.out.println("GfG1");
+    System.out.println("avvv");
+    System.out.println("GfG1");
 }
