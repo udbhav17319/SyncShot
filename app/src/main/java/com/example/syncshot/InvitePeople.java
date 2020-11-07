ecea0046fc7e186701f0ea6ce6e5bebfe3b01c4b
om-khandade <khandade18289@iiitd.ac.in>
2020-11-08 00:10:27 +0530

    Add invite people screen

---
+++ app/src/main/java/com/example/syncshot/InvitePeople.java
@@ -0,0 +1,14 @@
+package com.example.syncshot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InvitePeople extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invite_people);
+    }
+}
--- app/src/main/java/com/example/syncshot/MainActivity.java
+++ app/src/main/java/com/example/syncshot/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.syncshot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Button button = findViewById(R.id.add_people_button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                
+                startActivity(new Intent(MainActivity.this, InvitePeople.class));
             }
         });
 
